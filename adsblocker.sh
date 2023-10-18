67b4cc875751eacbd4711b9ec136c39ed7ecc709
“thegreatpayload” <huntprofessor@gmail.com>
2023-10-19 02:12:17 +0530

    update adsblocker

--- adsblocker.sh
+++ adsblocker.sh
@@ -10,10 +10,10 @@ apt-get install -y bind9
 
 echo "....Copying Configuration Fles..."
 cp named.conf /etc/bind/named.conf
-cp adsbloker.py /etc/bind/adsblocker.py
+cp adsblocker.py /etc/bind/adsblocker.py
 
 echo "....Fetch updated domain list..."
-python3 adsbloker.py
+python3 adsblocker.py
 
 echo "...Restart Services..."
 systemctl restart named
@@ -25,11 +25,11 @@ named-checkconf rpz /etc/bind/adsblocker.db
 
 #Schedule Cron Job to fetch updated domain list
 echo "...set Cronjob for future update..."
-crontab -l > adblocker
+crontab -l > adsblocker
 #cron job which will run at 00:00 on Sunday.
-echo "0 0 * * 0 sudo python3 /etc/bind/adsblocker.py >> /etc/bnd/adsblocker.log" >> adblocker
+echo "0 0 * * 0 sudo python3 /etc/bind/adsblocker.py>> /etc/bnd/adsblocker.log" >> adsblocker
 #install new cron file
 crontab adblocker
 
-rm adblocker
+rm adsblocker
 echo "...Finish... "
